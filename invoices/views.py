1ed5fa084fe7a4a84b0002736d3181976bdfc606
Fran Marín Pérez <franmp97@outlook.com>
2025-04-02 15:14:25 +0200

    JSON output and reformat

--- invoices/views.py
+++ invoices/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
+from django.db.models import Sum, Count
+from django.db.models.functions import ExtractYear, ExtractMonth
 from .models import Invoice
+from django.http import JsonResponse
 
 def invoice_list(request):
+
+    invoices = Invoice.objects.values(
+        'customer_name', 
+        'revenue_source_name',
+        'currency_code',
+        'adjusted_gross_value',
+        'haircut_percent',
+        'daily_advance_fee',
+        'advance_duration',
+        'invoice_date'
+    ).order_by('customer_name', '-invoice_date', 'revenue_source_name')
+
     customer_name = request.GET.get('customer_name', None)
-
     if customer_name:
-        invoices = Invoice.objects.filter(customer_name__icontains=customer_name)
+        invoices = invoices.filter(customer_name__icontains=customer_name)
     else:
-        invoices = Invoice.objects.all()
+        invoices = invoices.all()
 
-    return render(request, 'invoice_list.html', {'invoices': invoices, 'customer_name': customer_name})
+    if request.headers.get('Accept') == 'application/json':
+        return JsonResponse(list(invoices), safe=False)
+    return render(request, 'invoice_list.html', {'invoices': invoices})
