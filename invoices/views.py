6e85c35acb9cda5c0df389fa1a7427570b833c09
Fran Marín Pérez <franmp97@outlook.com>
2025-04-03 18:39:29 +0200

    count working

--- invoices/views.py
+++ invoices/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Case, CharField, Count, DecimalField, JSONField, Sum, Value, When
+from django.db.models import Case, CharField, Count, DecimalField, JSONField, IntegerField, Sum, Value, When, Avg
 from django.db.models.functions import Coalesce, ExtractMonth, ExtractYear
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -14,16 +14,19 @@ context = {
 
 def get_invoices_year(invoices_info, customer_id_query):
     context['view_type'] = 'years'
+    context['selected_customer'] = invoices_info.values_list('customer_name', flat=True).first()
+
     invoices_info = invoices_info.filter(customer_id=customer_id_query).annotate(
         year=ExtractYear('invoice_date'),
         total_invoices=Count('id')
     ).order_by('-year')
     
-    context['selected_customer'] = invoices_info.values_list('customer_name', flat=True).first()
     return invoices_info
 
 def get_invoices_month(invoices_info, year_query):
     context['view_type'] = 'months'
+    context['selected_year'] = invoices_info.values_list('year', flat=True).first()
+
     invoices_info = invoices_info.filter(year=year_query).annotate(
         month_id=ExtractMonth('invoice_date'),
         month_name=Case(
@@ -43,8 +46,6 @@ def get_invoices_month(invoices_info, year_query):
         ),
         total_invoices=Count('id')
     ).order_by('-month_id')
-
-    context['selected_year'] = invoices_info.values_list('year', flat=True).first()
     return invoices_info
 
 def unify_invoice_sources(data):
@@ -54,17 +55,22 @@ def unify_invoice_sources(data):
         unified_sources = {}
         
         for source in invoice_info["invoice_sources"]:
-            key = (source["revenue_source_name"], source["currency_code"])
-            
+            key = source["revenue_source_name"], source["currency_code"]
             if key in unified_sources:
                 unified_sources[key]["total_adjusted_gross_value"] += source["total_adjusted_gross_value"]
-                unified_sources[key]["invoice_count"] += 1
+                unified_sources[key]["total_haircut_percentage"] += source["total_haircut_percentage"]
+                unified_sources[key]["total_daily_advance_fee"] += source["total_daily_advance_fee"]
+                unified_sources[key]["total_advance_duration"] += source["total_advance_duration"]
+                unified_sources[key]["total_invoices"] += 1
             else:
                 unified_sources[key] = {
                     "revenue_source_name": source["revenue_source_name"],
                     "currency_code": source["currency_code"],
                     "total_adjusted_gross_value": source["total_adjusted_gross_value"],
-                    "invoice_count": 1
+                    "total_invoices": source["total_invoices"],
+                    "total_haircut_percentage": source["total_haircut_percentage"],
+                    "total_daily_advance_fee": source["total_daily_advance_fee"],
+                    "total_advance_duration": source["total_advance_duration"],
                 }
         
         unified_list = list(unified_sources.values())
@@ -80,6 +86,8 @@ def unify_invoice_sources(data):
 
 def get_invoices_info(invoices_info, month_query):
     context['view_type'] = 'invoice_info'
+    context['selected_month'] = invoices_info.values_list('month_name', flat=True).first()
+    
     invoices_info = invoices_info.filter(month_id=month_query)
 
     total_amount_by_source_revenue = (
@@ -87,19 +95,33 @@ def get_invoices_info(invoices_info, month_query):
         .values('revenue_source_name', 'currency_code')
         .annotate(
             total_adjusted_gross_value=Coalesce(
-                Sum('adjusted_gross_value'), 0, output_field=DecimalField()))
-        .order_by('-adjusted_gross_value')
+                Sum('adjusted_gross_value'), 0, output_field=DecimalField()
+            ),
+            total_haircut_percentage=Coalesce(
+                Avg('haircut_percent'), 0, output_field=DecimalField()
+            ),
+            total_daily_advance_fee=Coalesce(
+                Avg('daily_advance_fee'), 0, output_field=DecimalField()
+            ),
+            total_advance_duration=Coalesce(
+                Sum('advance_duration'), 0, output_field=IntegerField()
+            ),
+            total_invoices=Count('id')
+        )
+        .order_by('-total_adjusted_gross_value')
     )
 
     for entry in total_amount_by_source_revenue:
         entry['total_adjusted_gross_value'] = float(round(entry['total_adjusted_gross_value'], 2))
+        entry['total_haircut_percentage'] = float(round(entry['total_haircut_percentage'], 2))
+        entry['total_daily_advance_fee'] = float(round(entry['total_daily_advance_fee'], 2))
 
         invoices_info = invoices_info.annotate(
             invoice_sources=Value(list(total_amount_by_source_revenue), output_field=JSONField())
         )
 
-    context['selected_month'] = invoices_info.values_list('month_name', flat=True).first()
-    return unify_invoice_sources(invoices_info)
+    invoices_info = unify_invoice_sources(invoices_info)
+    return invoices_info
 
 
 def get_customers(request):
