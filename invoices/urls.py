b4e934b822e849cb2f2168f3944348279d632902
Fran Marín Pérez <franmp97@outlook.com>
2025-04-03 16:12:46 +0200

    sorting by revenue_source

--- invoices/urls.py
+++ invoices/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_invoices_info, name='get_invoices_info'),
+    path('', views.get_customers, name='get_customers'),
 ]
