57b0b19f68ed64236081551e308beb42ec9903aa
Fran Marín Pérez <franmp97@outlook.com>
2025-04-05 02:33:43 +0200

    views refactored

--- invoices/views.py
+++ invoices/views.py
@@ -1,8 +1,25 @@
-from django.db.models import Case, CharField, Count, DecimalField, JSONField, Value, When
-from django.db.models.functions import Coalesce, ExtractMonth, ExtractYear
+from django.db.models import Case, CharField, Count, JSONField, Value, When
+from django.db.models.functions import ExtractMonth, ExtractYear
 from django.http import JsonResponse, Http404
 from django.shortcuts import render
 from .models import Invoice
+
+month_map = (
+    Case(
+        When(month_id=1, then=Value("January")),
+        When(month_id=2, then=Value("February")),
+        When(month_id=3, then=Value("March")),
+        When(month_id=4, then=Value("April")),
+        When(month_id=5, then=Value("May")),
+        When(month_id=6, then=Value("June")),
+        When(month_id=7, then=Value("July")),
+        When(month_id=8, then=Value("August")),
+        When(month_id=9, then=Value("September")),
+        When(month_id=10, then=Value("October")),
+        When(month_id=11, then=Value("November")),
+        When(month_id=12, then=Value("December")),
+            output_field=CharField())
+)
 
 context = {
     'invoices_info': None,
@@ -31,21 +48,7 @@ def get_invoices_month(invoices_info, year_query):
 
     invoices_info = invoices_info.filter(year=year_query).annotate(
         month_id=ExtractMonth('invoice_date'),
-        month_name=Case(
-            When(month_id=1, then=Value("January")),
-            When(month_id=2, then=Value("February")),
-            When(month_id=3, then=Value("March")),
-            When(month_id=4, then=Value("April")),
-            When(month_id=5, then=Value("May")),
-            When(month_id=6, then=Value("June")),
-            When(month_id=7, then=Value("July")),
-            When(month_id=8, then=Value("August")),
-            When(month_id=9, then=Value("September")),
-            When(month_id=10, then=Value("October")),
-            When(month_id=11, then=Value("November")),
-            When(month_id=12, then=Value("December")),
-            output_field=CharField(),
-        ),
+        month_name=month_map,
         total_invoices=Count('id')
     ).order_by('-month_id')
 
@@ -53,46 +56,53 @@ def get_invoices_month(invoices_info, year_query):
         raise Http404("No invoices found for the given year.")
     return invoices_info
 
-def unify_invoice_sources(data):
+def merge_invoice_sources(data):
     result = []
     
     for invoice_info in data:
         unified_sources = {}
         
-        # Initialize unified sources
+        # Merging by revenue source name and currency code
         for entry in invoice_info["invoice_sources"]:
             key = entry["revenue_source_name"], entry["currency_code"]
             if key in unified_sources:
-                unified_sources[key]["total_adjusted_gross_value"] += entry["adjusted_gross_value"]
-                unified_sources[key]["monthly_haircut_percent"] += entry["haircut_percent"]
-                unified_sources[key]["monthly_advance_fee"] += entry["daily_advance_fee"]
-                unified_sources[key]["monthly_advance_duration"] += entry["advance_duration"]
-                unified_sources[key]["total_invoices"] += 1
+                source = unified_sources[key]
+                source["total_adjusted_gross_value"] += entry["adjusted_gross_value"]
+                source["monthly_haircut_percent"] += entry["haircut_percent"]
+                source["total_advance_fee"] += entry["daily_advance_fee"]
+                source["monthly_advance_duration"] += entry["advance_duration"]
+                source["total_invoices"] += 1
             else:
-                unified_sources[key] = {
+                source = {
                     "revenue_source_name": entry["revenue_source_name"],
                     "currency_code": entry["currency_code"],
                     "total_adjusted_gross_value": entry["adjusted_gross_value"],
-                    "total_invoices": entry["total_invoices"],
+                    "total_invoices": 1,
                     "monthly_haircut_percent": entry["haircut_percent"],
-                    "monthly_advance_fee": entry["daily_advance_fee"],
+                    "total_advance_fee": entry["daily_advance_fee"],
                     "monthly_advance_duration": entry["advance_duration"],
-                    "available_advance": entry["available_advance"],
+                    "available_advance": 0.0,
+                    "monthly_fee_amount": 0.0,
                 }
+                unified_sources[key] = source
     
-        # Calculate means for monthly params, available advance and daily fee amount
+        # Calculate for each merged source
         for entry in unified_sources:
-            unified_sources[entry]["monthly_haircut_percent"] = float(round(
-                unified_sources[entry]['monthly_haircut_percent'] / unified_sources[entry]['total_invoices'], 2
+            source = unified_sources[entry]
+            source["monthly_haircut_percent"] = float(round(
+                source['monthly_haircut_percent'] / source['total_invoices'], 2
             ))          
-            unified_sources[entry]["available_advance"] = float(round(
-                unified_sources[entry]['total_adjusted_gross_value'] * (1 - unified_sources[entry]['monthly_haircut_percent'] / 100), 2
-            ))
-            unified_sources[entry]["monthly_advance_duration"] = round(
-                unified_sources[entry]['monthly_advance_duration'] / unified_sources[entry]['total_invoices']
+            source["monthly_advance_duration"] = round(
+                source['monthly_advance_duration'] / source['total_invoices']
             )
+            source["available_advance"] = float(round(
+                source['total_adjusted_gross_value'] * (1 - source['monthly_haircut_percent'] / 100), 2
+            ))
+            source["monthly_fee_amount"] = float(round(
+                source['available_advance'] * (source['total_advance_fee'] / 100), 2
+            ))
   
-        # Convert unified sources to list and sort
+        # Convert merged sources to list and sort
         unified_list = list(unified_sources.values())
         unified_list.sort(key=lambda x: x["total_adjusted_gross_value"], reverse=True)
         
@@ -113,7 +123,7 @@ def get_invoices_info(invoices_info, month_query):
     if not invoices_info.exists():
         raise Http404("No invoices found for the given month.")
 
-    total_amount_by_source_revenue = (
+    list_source_revenue_info = (
         invoices_info
         .values('revenue_source_name',
                 'adjusted_gross_value',
@@ -124,48 +134,44 @@ def get_invoices_info(invoices_info, month_query):
                 )
         .annotate(
             total_invoices=Count('id'),
-            available_advance=Coalesce(0, 0, output_field=DecimalField()),
         )
         .order_by('-adjusted_gross_value')
     )
 
-    for entry in total_amount_by_source_revenue:
+    for entry in list_source_revenue_info:
         entry['adjusted_gross_value'] = float(round(
             entry['adjusted_gross_value'], 2))
         entry['haircut_percent'] = float(round(
             entry['haircut_percent'], 2))
         entry['daily_advance_fee'] = float(round(
             entry['daily_advance_fee'], 2))
-        entry['available_advance'] = float(round(
-            entry['available_advance'], 2))
 
     invoices_info = invoices_info.annotate(
-        invoice_sources=Value(list(total_amount_by_source_revenue), output_field=JSONField())
+        invoice_sources=Value(list(list_source_revenue_info), output_field=JSONField())
     )
 
-    invoices_info = unify_invoice_sources(invoices_info)
-    return invoices_info
+    return merge_invoice_sources(invoices_info)
 
-def get_customers(request):
+def get_invoices(request):
     # Validate request
     if not request:
         return JsonResponse({"error": "Invalid request"}, status=400)
     if request.method != 'GET':
         return JsonResponse({"error": "GET request required"}, status=400)
     
-    # Get query parameters
+    # Get query parameters from request
     customer_id_query = request.GET.get('customer_id', None)
     year_query = request.GET.get('year', None)
     month_query = request.GET.get('month', None)
     
     # Initialize context
+    context['view_type'] = 'customers'
     invoices_info = Invoice.objects.values(
         'customer_name', 
         'customer_id',
     ).annotate(
         total_invoices=Count('id')
     ).order_by('customer_name').distinct()
-    context['view_type'] = 'customers'
 
     if not invoices_info.exists():
         raise Http404("No invoices found.")
