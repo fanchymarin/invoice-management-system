368dad8c17c5d532eb266302f29f4351cb81a388
Fran Marín Pérez <franmp97@outlook.com>
2025-04-07 20:33:14 +0200

    currency formatted

--- src/app/settings.py
+++ src/app/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
 	'invoices.apps.InvoicesConfig',
+	'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
